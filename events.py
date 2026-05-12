d4732f819f5fb394749d8437abcbdcf1ce7a19d8
joeljojojochire-lgtm <joeljojojochire@gmail.com>
2026-05-12 22:02:34 +0200

    Update events.py

--- events.py
+++ events.py
@@ -1,14 +1,13 @@
 SPECIAL_CELLS = {
-
-    8: ("Te caes de una moto.", 3),
-    19: ("Atajo secreto.", 29),
-    34: ("Lodo tóxico.", 27),
-    46: ("Un fan te carga.", 58),
-    63: ("Pelea callejera.", 55),
-    71: ("Encuentras turbo ilegal.", 82),
-    89: ("Explosión radioactiva.", 77),
-    102: ("Te pierdes buscando WiFi.", 91),
-    113: ("Recta final VIP.", 118),
+    5: ("¡Mister Pipa te regala un batido energético!", 10),
+    12: ("Te detienes a firmar autógrafos y pierdes el ritmo.", 8),
+    19: ("¡Atajo por las alcantarillas! Apareces mucho más adelante.", 35),
+    28: ("Un viento fuerte te empuja hacia atrás.", 20),
+    34: ("¡Lodo pegajoso! Te cuesta salir de aquí.", 27),
+    46: ("Un fan emocionado te carga en hombros.", 55),
+    58: ("Te equivocas de camino en la selva.", 50),
+    65: ("¡Encuentras un patinete abandonado!", 75),
+    82: ("¡Turbo ilegal activado! ¡Vuela!", 95),
+    94: ("¡Un bache gigante! Retrocedes por el golpe.", 85),
+    105: ("Recta final: ¡La adrenalina te hace correr!", 115),
 }
-
-FREE_ITEM_CELLS = [15, 39, 67, 96]
