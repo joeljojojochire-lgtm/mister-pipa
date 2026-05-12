b588e20540b3a0dfd3e85566f55a5c4c75686012
joeljojojochire-lgtm <joeljojojochire@gmail.com>
2026-05-12 21:46:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,18 @@ MAP_SIZES = {
     3: 85,
     4: 100,
     5: 110,
+# --- CONFIGURACIÓN VISUAL 2.0 ---
+HEADER_IMAGE = "https://i.ibb.co/vzYm8m8/mister-pipa-header.jpg" # Sustituir por tu URL real
+MARQUEE_TEXT = "ღ(¯◕‿◕´¯) ♫ ♪ ♫ mister pipa ♫ ♪ ♫ (¯◕‿◕´¯)ღ"
+
+PIPA_EMOJIS = {
+    "default": "😀",
+    "roll": "😄",
+    "sabotage": "😆",
+    "joke": "😂",
+    "boost": "😇",
+    "vote": "🧐",
+    "result": "🫡"
 }
+
+PLAYER_EMOJIS = ["🚗", "🚲", "🦖", "🛸", "🏄", "👻", "🤖", "🐥", "🐒", "🦄", "🏎️", "🚁"]
