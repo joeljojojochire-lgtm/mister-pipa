fc2c988693ed770c098461edfbfd0cf68f901296
joeljojojochire-lgtm <joeljojojochire@gmail.com>
2026-05-13 21:10:27 +0200

    Create dialogos.py

---
+++ dialogos.py
@@ -0,0 +1,69 @@
+# dialogos.py - El guion de Mister Pipa
+
+DIALOGOS = {
+    "inicio": [
+        "1. ¡Mis patitas están vibrando! ¡Corran por sus vidas!",
+        "2. animo pobres! vamos a hacer dinero 🫰🤑",
+        "3. spoiler, nadie gana!",
+        "4. el monte everest no tiene en nada en contra de mi",
+        "5. vale todo menos llorar 😭"
+    ],
+    "sacar_6": [
+        "1. ¡Esa velocidad es ilegal! ¿Llevas propulsores?",
+        "2. trampa, no tengo pruebas , pero tampco dudas",
+        "3. Corre como ladron de celular",
+        "4. para correr si estan buenos",
+        "5. el rapido le dicen",
+        "6. Corre billy!!! corre!!!!",
+        "7. la de parar no te la sabes papito?",
+        "8. agarrenlo, se robo mi bicicleta 🚲!!"
+    ],
+    "sacar_1": [
+        "1. ¿Eso fue un dado o un bostezo? ¡Muévete!",
+        "2. no esperaba menos de ti, pero aun asi logras decepcionarme",
+        "3. No te vayas a romper",
+        "4. si vas rapido, solo que de a poquito",
+        "5. dale, asi. pero al revés 🙃",
+        "6. No eresnlento, es estrategia",
+        "7. asi campeón",
+        "8. Todavia sirve"
+    ],
+    "votacion_abierta": [
+        "1. Huelo miedo... y me gusta. ¿A quién vamos a hundir?",
+        "2. Como no pasa nada, hay que ajusticiarse a alguien",
+        "3. ",
+        "4. [ESCRIBE AQUÍ]",
+        "5. [ESCRIBE AQUÍ]",
+        "6. [ESCRIBE AQUÍ]",
+        "7. [ESCRIBE AQUÍ]",
+        "8. [ESCRIBE AQUÍ]",
+        "9. [ESCRIBE AQUÍ]",
+        "10. [ESCRIBE AQUÍ]"
+    ],
+    "trampa_caida": [
+        "1. ¡Zas! Al barro. Espero que ese traje fuera barato.",
+        "2. [ESCRIBE AQUÍ]",
+        "3. [ESCRIBE AQUÍ]",
+        "4. [ESCRIBE AQUÍ]",
+        "5. [ESCRIBE AQUÍ]",
+        "6. [ESCRIBE AQUÍ]",
+        "7. [ESCRIBE AQUÍ]",
+        "8. [ESCRIBE AQUÍ]",
+        "9. [ESCRIBE AQUÍ]",
+        "10. [ESCRIBE AQUÍ]"
+    ],
+    "recta_final": [
+        "1. ¡Huelo el éxito! Y un poco de sudor también.",
+        "2. [ESCRIBE AQUÍ]",
+        "3. [ESCRIBE AQUÍ]",
+        "4. [ESCRIBE AQUÍ]",
+        "5. [ESCRIBE AQUÍ]"
+    ],
+    "comentario_azar": [
+        "1. 🧐 Las cosas están bien, demasiado bien. Votemos...",
+        "2. [ESCRIBE AQUÍ]",
+        "3. [ESCRIBE AQUÍ]",
+        "4. [ESCRIBE AQUÍ]",
+        "5. [ESCRIBE AQUÍ]"
+    ]
+}
